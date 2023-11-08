64daf257f568480cdc9657d111c6772341489f56
sai prakash <prakashsaivenkat@gmail.com>
2023-11-08 11:17:57 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def fun2(response):
+    return HttpResponse('<h1><marquee>HI I AM GAYATHRI</h1></marquee>') 
